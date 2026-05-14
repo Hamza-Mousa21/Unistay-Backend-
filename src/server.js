fcc4d08c354380f710b5838ff3afeb6d4b318e12
majedhanini <najedalhanini604@gmail.com>
2026-05-15 02:09:12 +0300

    `Updated student authentication controller and server.js file with minor changes`

--- src/server.js
+++ src/server.js
@@ -1,30 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const db = require("../models");
+const cors = require("cors");
+const Ratingrouter = require("../routes/ratingRoutes");
+const wishListRouter = require("../routes/wishListRoutes");
+const studentRouter = require("../routes/studentAuthRoutes");
+const ownerRouter = require("../routes/ownerAuthRoutes");
+const residenceRouter = require("../routes/residenceRoutes");
 
+const PORT = 3000;
 
-const express=require('express')
-const app=express()
-const db=require('../models')
-const cors =require('cors')
-const Ratingrouter=require('../routes/ratingRoutes')
-const wishListRouter=require('../routes/wishListRoutes')
-const studentRouter=require('../routes/studentAuthRoutes')
-const ownerRouter=require('../routes/ownerAuthRoutes')
-const residenceRouter=require('../routes/residenceRoutes')
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
 
+app.use("/residence/:residenceId/Ratings", Ratingrouter);
+app.use("/residence/:residenceId/wishlist", wishListRouter);
+app.use("/residence", residenceRouter);
+app.use("/student", studentRouter);
+app.use("/owner", ownerRouter);
 
-
-const PORT=3000;
-
-
-app.use(cors({ origin: 'http://localhost:5173' }))
-app.use(express.json())
-
-
-app.use('/residence/:residenceId/Ratings',Ratingrouter)
-app.use('/residence/:residenceId/wishlist',wishListRouter)
-app.use('/residence',residenceRouter)
-app.use('/student',studentRouter)
-app.use('/owner',ownerRouter)
-
-app.listen(PORT,()=>{
-    console.log("Server is running")
-})
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
